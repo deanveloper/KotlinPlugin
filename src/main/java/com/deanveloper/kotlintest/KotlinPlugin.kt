8ec535d22687043863daa56b177318d8d8ec6e53
Dean Bassett <deanvelopermn@gmail.com>
2016-02-09 15:28:49 -0600

    Added a lambda example

--- src/main/java/com/deanveloper/kotlintest/KotlinPlugin.kt
+++ src/main/java/com/deanveloper/kotlintest/KotlinPlugin.kt
@@ -10,13 +10,20 @@ import org.bukkit.plugin.java.JavaPlugin
  */
 class KotlinPlugin: JavaPlugin() {
     //while this is singleton, a class must be initialized by Bukkit, so we can't use 'object'
+    companion object {
+        var instance: KotlinPlugin? = null;
+        private set;
+    }
 
     override fun onEnable() {
         getCommand("echo").executor = KotlinEchoCmd
         getCommand("player").executor = KotlinPlayerCmd
+        getCommand("jarjar").executor = KotlinTestRunnable
         Bukkit.getPluginManager().registerEvents(KotlinListener, this)
 
         Bukkit.getLogger().info("Config Val: ${config.getString("configVal") ?: "[no val listed]"}")
+
+        instance = this;
 
         Bukkit.getLogger().info("Enabled!")
     }
---
+++ src/main/java/com/deanveloper/kotlintest/KotlinTestRunnable.kt
@@ -0,0 +1,35 @@
+package com.deanveloper.kotlintest;
+
+import khttp.get
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+
+/**
+ * (description)
+ *
+ * @author (name)
+ */
+object KotlinTestRunnable: CommandExecutor {
+    override fun onCommand(sender: CommandSender, cmd: Command, lbl: String, args: Array<String>): Boolean {
+        val movie: Int;
+        try {
+            movie = Integer.getInteger(args[0]);
+            assert(movie in 1..7)
+        } catch (e: Exception) {
+            return false;
+        }
+        Bukkit.getScheduler().runTaskAsynchronously(KotlinPlugin.instance) {
+            val jarjar = get("http://swapi.co/api/people/36").jsonObject
+            val films = jarjar.getJSONArray("films");
+            if(films.contains("http://swapi.co/api/films/$movie/")) {
+                sender.sendMessage("That movie was bad, jarjar was in it")
+            } else {
+                sender.sendMessage("That movie was fine, jarjar wasn't in it")
+            }
+        }
+
+        return true;
+    }
+}
